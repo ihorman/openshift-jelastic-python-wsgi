afdbf12fd1b4d18d2d2bdb239424462f98dd4719
Ihor Kolodyuk <ik@jelastic.com>
2014-04-15 06:35:15 +0300

    fx

--- jelastic/scripts/deploy.sh
+++ jelastic/scripts/deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# Simple deploy and undeploy scenarios for Jetty8
+# Simple deploy and undeploy scenarios for Jelastic Python
+
+function _setContext(){
+        echo "You application just been deployed to wsgi context"
+}
+
 
 include php-common-deploy;  # functionality which is already implemented for PHP works for Python applications as well
 
